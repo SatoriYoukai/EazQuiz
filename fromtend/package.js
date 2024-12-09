46c598b88eecb69e6dfc2db817f3cfaccef43da7
SatoriYoukai <132784450+SatoriYoukai@users.noreply.github.com>
2024-12-09 16:42:58 +0800

    Create package.js
    from o1

---
+++ fromtend/package.js
@@ -0,0 +1,14 @@
+{
+  "name": "my-flashcard-frontend",
+  "version": "0.0.1",
+  "scripts": {
+    "dev": "vite",
+    "build": "vite build",
+    "preview": "vite preview"
+  },
+  "devDependencies": {
+    "@sveltejs/vite-plugin-svelte": "^x.x.x",
+    "svelte": "^3.x.x",
+    "vite": "^4.x.x"
+  }
+}
